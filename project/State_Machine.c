d0feb6706d4400d30028f8331a3fa7878add6738
Addy <amgalvan@miners.utep.edu>
2020-11-03 00:26:03 -0700

    added to state machine

--- project/State_Machine.c
+++ project/State_Machine.c
@@ -16,6 +16,21 @@ void state_advance(){
       current_state = MENU_3;
     }
     break;
+    // flashing red light really quickly to make it dim 
+  case MENU_0:
+    tempo = 1;
+    red_on = 1, green_on = 0;
+    led_update();
+    red_on = 0; green_on = 0;
+    led_update();
+    current_state = MENU_0;
+    if(side_switch){
+      current_state = START;
+    }
+  case MENU_1:
+  case MENU_2;
+  case MENU_3;
+  
   }
 }
     
