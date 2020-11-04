d21786bc138171159dffe74a3a5b086e55701a32
Addy <amgalvan@miners.utep.edu>
2020-11-04 15:50:46 -0700

    added switches

--- project/switches.h
+++ project/switches.h
@@ -1,12 +1,14 @@
 #ifndef switches_included
 #define switches_included
 
-#define SW1 BIT3 //switch1 is p1.3
-#define SWITCHES SW1 // only 1 switch on this board
-
+#define SW1 BIT0 //switch1 is p2.0
+#define SW2 BIT1 //switch2 is p2.1
+#define SW3 BIT2 //switch3 is p2.2
+#define SW4 BIT3 //switch4 is p2.3
+#define SWITCHES (SW1 | SW2 | SW3 | SW4) //the four switches
 void switch_init();
 void switch_interrupt_handler();
 
-extern char switch_state_down, switch_state_changed; // effectively boolean
+extern char switch_state_down, switch_state_changed, switch1, switch2, switch3, switch4; // effectively boolean
 
 #endif //included
