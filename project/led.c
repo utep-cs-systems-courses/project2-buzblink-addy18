0637815df0a0dd3232f8885bb83bfdf4f493b647
Addy <amgalvan@miners.utep.edu>
2020-11-04 15:17:47 -0700

    fixed bugs

--- project/led.c
+++ project/led.c
@@ -4,7 +4,7 @@
 
 void led_init(){
   P1DIR |= LEDS; //bits attached to leds are output
-  switch_state_chenged = 1;
+  switch_state_changed = 1;
   led_update();
 }
 
@@ -15,7 +15,7 @@ void led_update(){
     ledFlags |= switch_state_down ? LED_GREEN : 0;
     ledFlags |= switch_state_down ? 0 : LED_RED;
 
-    P1OUT &= (0xff - LEDS) | ledFLags; // clear bits for off leds
+    P1OUT &= (0xff - LEDS) | ledFlags; // clear bits for off leds
     P1OUT |= ledFlags; //set bits for on leds
   }
   switch_state_changed = 0;
