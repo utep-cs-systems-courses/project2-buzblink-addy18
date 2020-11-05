bbc33f185a07d87a260916219c0de9a7a6276e3a
Addy <amgalvan@miners.utep.edu>
2020-11-05 01:39:37 -0700

    cleaned it up

--- project/led.h
+++ project/led.h
@@ -7,10 +7,13 @@
 #define LED_GREEN BIT6 //p1.6
 #define LEDS (BIT0 | BIT6)
 
+extern unsigned char red_on, green_on;
+extern unsigned char led_changed;
+
 void led_init();  //initializa LEDs
 void led_update(); // update LEDs
 
 //these are boolean flags
-extern unsigned char green_led_state, red_led_state, red_on, green_on;
+extern unsigned char green_led_state, red_led_state, leds_changed;
 
 #endif
