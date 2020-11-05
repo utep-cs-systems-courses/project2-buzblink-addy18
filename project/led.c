4550697ec4f137406ea94bbc002f985633c687cb
Addy <amgalvan@miners.utep.edu>
2020-11-05 01:39:16 -0700

    fixed a few typos

--- project/led.c
+++ project/led.c
@@ -1,24 +1,29 @@
 #include <msp430.h>
 #include "led.h"
 #include "switches.h"
+#include "State_Machine.h"
 
 unsigned char red_on = 0, green_on = 0;
+unsigned char led_changed = 0;
+
 static char redVal[] = {0, LED_RED}, greenVal[] = {0, LED_GREEN};
 
 void led_init(){
   P1DIR |= LEDS; //bits attached to leds are output
-  switch_state_changed = 1;
+  led_changed = 1;
   led_update();
 }
 
 void led_update(){
-  char ledFlags = redVal[red_on] | greenVal[green_on];  // by default, no LEDs on
+  if(led_changed){
+    char ledFlags = redVal[red_on] | greenVal[green_on];  // by default, no LEDs on
 
-  //ledFlags |= switch_state_down ? LED_GREEN : 0;
-  //ledFlags |= switch_state_down ? 0 : LED_RED;
+    ledFlags |= switch_state_down ? LED_GREEN : 0;
+    ledFlags |= switch_state_down ? 0 : LED_RED;
 
-  P1OUT &= (0xff - LEDS) | ledFlags; // clear bits for off leds
-  P1OUT |= ledFlags; //set bits for on leds
-  
-  switch_state_changed = 0;
+    P1OUT &= (0xff^LEDS) | ledFlags; // clear bits for off leds
+    P1OUT |= ledFlags; //set bits for on leds
+    led_changed = 0;
+  }
+  //switch_state_changeed = 0;
 }
