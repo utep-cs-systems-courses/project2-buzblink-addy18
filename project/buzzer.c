31fc837cfdd2849569c18defb54d8c6e80ee41df
Addy <amgalvan@miners.utep.edu>
2020-11-05 01:38:51 -0700

    added notes and songs

--- project/buzzer.c
+++ project/buzzer.c
@@ -2,6 +2,20 @@
 #include "libTimer.h"
 #include "buzzer.h"
 
+//notes
+#define A3 2273
+#define B3 2025
+#define G3 2551
+#define E3 3033
+#define C3 3846
+#define C4 1911
+#define D4 1703
+#define E4 1517
+#define F4 1432
+#define G4 1276
+#define A4 1136
+#define B4 1012
+
 void buzzer_init(){
   /*
     Direct timer A output "TA0.1" to P2.6.
@@ -20,4 +34,36 @@ void buzzer_init(){
 void buzzer_set_period(short cycles){ // buzzer clock = 2MHz. (period of 1k results in 2kHz tone)
   CCR0 = cycles;
   CCR1 = cycles >>1; // one half cycle
+}
+
+void song1(){
+  int song[] = {A3, G3, F4, E3};
+  for(int i = 0; i < sizeof(song) / sizeof(*song); i++){
+    buzzer_set_period(song[i]);
+    __delay_cycles(850000);
+  }
+}
+
+void song2(){
+  int song[] = {B4, A4, F4};
+  for(int i = 0; i < sizeof(song) / sizeof(*song); i++){
+    buzzer_set_period(song[i]);
+    __delay_cycles(8500000);
+  }
+}
+
+void song3(){
+  int song[] = {G4, D4, C3, B3};
+  for(int i = 0; i < sizeof(song) / sizeof(*song); i++){
+    buzzer_set_period(song[i]);
+    __delay_cycles(8500000);
+  }
+}
+
+void song4(){
+  int song[] = {E4, C3, D4, A4, G3};
+  for(int i = 0; i < sizeof(song) / sizeof(*song); i++){
+    buzzer_set_period(song[i]);
+    __delay_cycles(8500000);
+  }
 }
